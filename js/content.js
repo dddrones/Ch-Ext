0601acf41b58298aac520200d2c09285b7a1255a
Jac <jac.miller@uleth.ca>
2021-02-24 03:59:07 -0700

    hellish

--- js/content.js
+++ js/content.js
@@ -1,7 +1,89 @@
-console.log("extension is running!")
+console.log("i'm here, i'm here!!!");
 
-// TRY: updating the text content of an element or set of elements on one or more pages
+// if (navigator.storage && navigator.storage.persist) {
+//   navigator.storage.persist().then(granted => {
+//     if (granted)
+//       alert("Storage will not be cleared except by explicit user action");
+//     else
+//       alert("Storage may be cleared by the UA under storage pressure.");
+//   });
+// }
 
-// TRY: adding a click event listener to an element that changes the background of that element to a new color
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	console.log(request.message1);
+  	var imgf1 = request.message1;
+  	// $('#planted').parent().append("<img	src="	+	imgf1	+	"	class='planted'>");
+  	$("header").after("<img	src="	+	imgf1	+	"	class='planted'>");
+  	$imgp1.hide();
+  }
+);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	console.log(request.message2);
+  	var imgp1 = request.message2;
+  	// $('#planted').parent().append("<img	src="	+	imgf1	+	"	class='planted'>");
+  	$("header").after("<img	src="	+	imgp1	+	"	class='planted'>");
+  }
+);
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	console.log(request.message3);
+  	var imgs1 = request.message3;
+  	// $('#planted').parent().append("<img	src="	+	imgf1	+	"	class='planted'>");
+  	$("header").after("<img	src="	+	imgs1	+	"	class='planted'>");
+  }
+);
 
-// TRY: using a mouseenter event listener to update text to something new.
+
+// $("body").on("click", function() {
+// 	$("body").css("background-color", "#7C8557")
+
+// });
+
+// $("body").on("click", function() {
+//     $("body").css("background-color", "#7C8557")
+//     $("div.o3j99.n1xJcf.Ne6nSd").after("color1")
+// });
+
+
+
+
+// var img = new Image();
+// img.src = 'images/f_1.png';
+// img.load = function() {
+// 	var canvas = document.createElement('canvas');
+// 	document.body.appendChild(canvas);
+// 	var contect = canvas.getContext('2d');
+// 	context.drawImage(img, 0, 0);
+// 	var data = context.getImageData(x, y, img.width, img.height).data;
+// 	localStorage.setItem('image', data);
+// };
+
+// window.onload = function() {
+//  var picture = localStorage.getItem('image');
+//  var image = document.createElement('img');
+//  image.src = picture;
+//  document.body.appendChild(image);
+// };
+
+
+// var imgURL = chrome.runtime.getURL("images/f_final.png");
+// var img2 = chrome.runtime.getURL("images/f_wilted.png");
+
+// $("header").before("<img src=" + imgURL + "class='body'>");
+
+// $("header").before("<img src=" + imgURL + "class='body'>");
+
+// $img1.on("click", function() {
+// 	$(this).hide();
+// 	$img2.show();
+// });
+
+// $imgURL.show();
+
+//so we want an image that will change to another image over a certain amount of time
+//will have 6 imgs per plant (18 total)
+//$one.on("click", function() { (for changing from wilted to watered)
+// 	$(this).hide(); 
+// 	$two.show();
---
+++ js/popup.js
@@ -0,0 +1,28 @@
+console.log("hello hello!")
+
+	var imgf1 = chrome.runtime.getURL("../images/f_final.png");
+	var imgp1 = chrome.runtime.getURL("../images/p_final.png");
+	var imgs1 = chrome.runtime.getURL("../images/s_final.png");
+
+
+$('#fbutton').on('click', fplant);
+  	$("imgp1").hide();
+function fplant () {
+		chrome.tabs.query({active:	true,	currentWindow:	true},	function(tabs)	{
+	 		chrome.tabs.sendMessage(tabs[0].id,	{message1: imgf1});
+});
+}
+
+$('#pbutton').on('click', pplant);
+function pplant () {
+		chrome.tabs.query({active:	true,	currentWindow:	true},	function(tabs)	{
+	 		chrome.tabs.sendMessage(tabs[0].id,	{message2: imgp1});
+});
+}
+
+$('#sbutton').on('click', splant);
+function splant () {
+		chrome.tabs.query({active:	true,	currentWindow:	true},	function(tabs)	{
+	 		chrome.tabs.sendMessage(tabs[0].id,	{message3: imgs1});
+});
+}
